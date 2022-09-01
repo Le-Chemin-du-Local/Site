a15313512fd0ff299d2b7b35aa73658b85fa790d
Feldrise <admin@feldrise.com>
2022-09-01 18:04:54 +0200

    🚚 Déplacement des fichiers dans src

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import {AppProps} from 'next/app';
-import '../styles/globals.css';
+import '../../styles/globals.css';
 
 /**
  * L'objet application
  * @param {{Component, pageProps}} param0 Les paramètres passé à l'application
- * @return {AppProps} le HTML de l'application
+ * @return {JSX.Element} le HTML de l'application
  */
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
 	return <Component {...pageProps} />;
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 module.exports = {
 	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
+		'./src/pages/**/*.{js,ts,jsx,tsx}',
+		'./src/components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary-color': '#ff5858',
+				'secondary-color': '#ff8c60',
+				'primary-color-lightened': '#ffc9c9',
+				'secondary-color-lightened': '#ffc9b2',
+
+				'light-grey-1': '#f4f4f4',
+				'light-grey-2': '#dadada',
+				'light-grey-3': '#bbbbbb',
+				'dark-grey': '#454545',
+
+				'error': '#e34141',
+				'error-container': '#ffadad',
+				'success': '#519B53',
+				'success-container': '#B8FFBA',
+				'warning-container': '#F4BD2A',
+				'information-container': '#36A2B1',
+			},
+		},
 	},
 	plugins: [],
 };
