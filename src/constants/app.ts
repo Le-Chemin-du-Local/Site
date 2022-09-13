6c9d47ef2eb468c6703f7ddf7b6f3c3393c01f83
Feldrise <admin@feldrise.com>
2022-09-13 16:40:54 +0200

    👽️ Correction de l'URL d'API en prod

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -3,7 +3,7 @@ export const DEV_MODE = process.env.NODE_ENV !== 'production';
 
 // URL Application
 export const API_URL_DEV = 'http://localhost:8082/query';
-export const API_URL_PROD = 'https://beta-api.chemin-du-local.bzh/query';
+export const API_URL_PROD = 'https://api-beta.chemin-du-local.bzh/query';
 
 // SECRETS
 export const COOKIE_SECRET = process.env.COOKIE_SECRET;
